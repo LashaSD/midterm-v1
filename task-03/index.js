6ce510bbffeb94ee0ec2e9aa6a6912a71b2e0859
Lasha <90580742+LashaSD@users.noreply.github.com>
2025-04-11 16:49:21 +0400

    finish task-03

--- task-03/index.js
+++ task-03/index.js
@@ -27,3 +27,46 @@ const users = [
     avatar: "https://fakeimg.pl/150x150",
   },
 ];
+
+function userCreate(user) {
+  let card = document.createElement('div');
+  card.innerHTML = `
+    <div class="card">
+      <div class="card-header">
+        <div class="user-info">
+          <div class="avatar">
+            <img src="https://fakeimg.pl/150x150" alt="${user.name}" />
+          </div>
+          <div class="user-details">
+            <h3>${user.name}</h3>
+            <p class="user-email">${user.email}</p>
+          </div>
+        </div>
+      </div>
+      <div class="card-content">
+        <p class="user-id">User ID: ${user.id}</p>
+      </div>
+    </div>
+  `
+  return card;
+}
+
+const cardsContainer = document.querySelector('#userList');
+const btn = document.querySelector("#toggleButton")
+let btnToggle = false;
+
+btn.onclick = function() {
+  if (!btnToggle) {
+    users.forEach(user => {
+      const card = userCreate(user);
+      cardsContainer.appendChild(card);
+    });
+    btn.innerText = "Hide Users"
+  } else {
+    while (cardsContainer.hasChildNodes()) {
+      cardsContainer.removeChild(cardsContainer.firstChild);
+    }
+    btn.innerText = "Show Users"
+  }
+  btnToggle = !btnToggle;
+}
